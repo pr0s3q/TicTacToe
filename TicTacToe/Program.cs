6bf1b6d5f5c729ccc6b6f0590d43f2236f68545c
Krzysztof Pecka <pr0s3q@gmail.com>
2023-11-01 13:05:43 +0100

    Init

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+
+static class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        using (Game g = new Game())
+        {
+            new GraphicsDeviceManager(g);
+            g.Run();
+        }
+    }
+}
